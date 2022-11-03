e10e04d8e58ef4f66df1736d9ea6676ad13ab5c1
kozaneckiwojtek <kozaneckiwojtek@gmail.com>
2022-11-03 13:25:11 +0100

    Policz

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int Policz(int a,int b){
+    return a+b;
+}
+
+
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszy branch 2+3 =%d ", Policz(2,3));
     return 0;
 }
